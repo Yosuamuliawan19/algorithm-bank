df5fdfc83ac1f33d8a03cbe7e15addd7fb751d0c
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2020-10-30 08:05:58 +0000

    Sync LeetCode submission - Reverse Linked List (cpp)

---
+++ Leetcode_submissions/problems/reverse_linked_list/solution.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) return head;
+        ListNode *prev = nullptr, *cur = head;
+        while (cur->next != nullptr){
+            ListNode* nextTemp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = nextTemp;
+        }
+        cur->next = prev;
+        return cur;
+    }
+};

eb465f686242a819829f22e89c878701d120db91
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2020-12-12 10:04:05 +0000

    Sync LeetCode submission - Divisor Game (cpp)

---
+++ Leetcode_submissions/problems/divisor_game/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool divisorGame(int N) {
+        
+        
+//         A 4 2 1
+            
+        
+//         B 3 1        B 2 1
+          
+//         A 2 1        A 1
+    
+//         B 1
+        return N % 2 == 0;
+    }
+};

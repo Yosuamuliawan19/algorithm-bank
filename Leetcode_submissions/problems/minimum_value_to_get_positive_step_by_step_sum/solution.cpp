25c38dd0b3832fbe7f9057df6acc6d6077cfae8d
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2020-12-13 11:57:20 +0000

    Sync LeetCode submission - Minimum Value to Get Positive Step by Step Sum (cpp)

---
+++ Leetcode_submissions/problems/minimum_value_to_get_positive_step_by_step_sum/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minStartValue(vector<int>& nums) {
+        int sum = 0, mn = INT_MAX;
+        for (auto i: nums){
+             sum += i;
+            mn = min(mn, sum);
+        }
+        return max(1,1 + (-mn)) ;
+    }
+};

6f97a514e84dda3cc83bda0ae3263fbca48783ab
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2020-12-17 11:11:56 +0000

    Sync LeetCode submission - Find Words That Can Be Formed by Characters (cpp)

--- Leetcode_submissions/problems/find_words_that_can_be_formed_by_characters/solution.cpp
+++ Leetcode_submissions/problems/find_words_that_can_be_formed_by_characters/solution.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     int countCharacters(vector<string>& words, string chars) {
+        int arr[26] = {0};
+        for (char c: chars){
+            arr[c-'a'] ++;
+        }
         int ans = 0;
-        int arr[26] = {0}, arr2[26] = {0};
-        for (char c: chars) arr[c-'a'] ++;
-        for (auto word: words){
-            memset(arr2, 0, sizeof(arr2));
-            for (char c: word){
-                arr2[c-'a']++;
+        for (int i=0;i<words.size();i++){
+            int arr2[26] = {0};
+            for (int j=0;j<words[i].size();j++){
+                arr2[words[i][j]-'a']++;    
             }
-            bool suc = 1;
-            for (int i=0;i<26;i++){
-                if (arr2[i] > arr[i]){
-                    suc = 0; break;
+            bool f = 1;
+            for (int j=0;j<26;j++){
+                if (arr2[j] > arr[j]){
+                    f = 0; break;
                 }
             }
-            if (suc) ans += word.length();
+            if (f) ans += words[i].length();
         }
         return ans;
+       
     }
 };

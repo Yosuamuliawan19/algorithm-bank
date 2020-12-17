830a4200c6e18c3e495aeb97c4a9ff62aa3d7df6
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2020-12-17 07:41:24 +0000

    Sync LeetCode submission - Pascal's Triangle (cpp)

--- Leetcode_submissions/problems/pascal's_triangle/solution.cpp
+++ Leetcode_submissions/problems/pascal's_triangle/solution.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> ans;
-        int len = 1;
-        for (int i=0;i<numRows;i++){
-            cout << i << endl;
-            vector<int> row;
-            for (int j=0;j<len;j++){
-                if (j == 0 || j == len-1) row.push_back(1);
-                else row.push_back(ans[i-1][j] + ans[i-1][j-1]);
+        for (int i=1;i<=numRows;i++){
+            ans.push_back(vector<int>(i, 1));
+            if (i >= 3){
+                for (int j=1;j<i-1;j++){
+                    // cout << j << " " ;
+                    ans[i-1][j] = ans[i-2][j-1] + ans[i-2][j];
+                }
+                // cout << endl;
             }
-            ans.push_back(row);
-            len ++;
         }
+        
         return ans;
     }
 };

9f18e180df65ba29512a0b616ba2dfebd1c97d6b
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2021-01-26 08:42:35 +0000

    Sync LeetCode submission - Number of Recent Calls (javascript)

---
+++ Leetcode_submissions/problems/number_of_recent_calls/solution.js
@@ -0,0 +1,22 @@
+
+var RecentCounter = function() {
+    this.arr = [];
+};
+
+/** 
+ * @param {number} t
+ * @return {number}
+ */
+RecentCounter.prototype.ping = function(t) {
+    this.arr.push(t);
+    let idx = 0;
+    while (t -  this.arr[idx] > 3000) idx++;
+    this.arr = this.arr.slice(idx);
+    return this.arr.length;
+};
+
+/** 
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = new RecentCounter()
+ * var param_1 = obj.ping(t)
+ */

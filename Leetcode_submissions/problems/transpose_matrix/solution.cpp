d0c8c4e2f879830a399a9baa58caec5c1c422236
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2021-02-09 14:21:37 +0000

    Sync LeetCode submission - Transpose Matrix (cpp)

---
+++ Leetcode_submissions/problems/transpose_matrix/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& A) {
+        vector<vector<int>> B(A[0].size(), vector<int>(A.size(), 0));
+        for (int i=0;i<A.size();i++){
+            for (int j=0;j<A[i].size();j++){
+                B[j][i] = A[i][j] ;
+            }
+        }
+        return B;
+    }
+};

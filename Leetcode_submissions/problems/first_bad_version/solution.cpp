e36217122aac44d093303a24d517c686b778d075
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2021-06-06 03:30:05 +0000

    Sync LeetCode submission - First Bad Version (cpp)

--- Leetcode_submissions/problems/first_bad_version/solution.cpp
+++ Leetcode_submissions/problems/first_bad_version/solution.cpp
@@ -4,16 +4,15 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int l = 1, r = n, ans;
-        while (r >= l){
+        int l = 0, r = n; 
+        while (r > l){
             int m = l + (r-l)/2;
             if (isBadVersion(m)){
-                r = m - 1;
-                ans = m;
+                r = m;
             }else{
-                l = m + 1;
+                l = m+1; 
             }
         }
-        return ans;
+        return l;
     }
 };

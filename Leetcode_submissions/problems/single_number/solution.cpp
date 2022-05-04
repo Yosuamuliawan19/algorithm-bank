d82283e3918039686806defefba685385aecf203
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2022-05-04 15:11:23 +0000

    Sync LeetCode submission - Single Number (cpp)

--- Leetcode_submissions/problems/single_number/solution.cpp
+++ Leetcode_submissions/problems/single_number/solution.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int a = 0;
-        for (int i: nums){
-            a ^= i;
-            
-        }return a;
+        map<int,int> mp;
+        
+        for (int i=0;i<nums.size();i++){
+            if (mp.count(nums[i]) == 0){
+                mp[nums[i]] = i;
+            }else{
+                nums[i] = nums[mp[nums[i]]] = (-3*10000)-1;
+            }
+        }
+        
+        
+        for (int i=0;i<nums.size();i++){
+            if (nums[i] != (-3*10000)-1){
+                return nums[i];
+            }
+        }
+        return -1;
+        
     }
 };

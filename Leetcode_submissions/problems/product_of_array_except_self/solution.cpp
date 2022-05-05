813d07d3a9507a88d755afa7404f3b782fd43438
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2022-05-05 14:20:07 +0000

    Sync LeetCode submission - Product of Array Except Self (cpp)

--- Leetcode_submissions/problems/product_of_array_except_self/solution.cpp
+++ Leetcode_submissions/problems/product_of_array_except_self/solution.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> a(nums.size()+1), b(nums.size()+1), ans(nums.size());
-        int n = nums.size();
-        a[0] = 1; b[n] = 1;
-        for (int i=1;i<n;i++){
-            a[i] = a[i-1] * nums[i-1];
+        vector<int> ans;
+        int total = 1, zeros = 0;
+        for (int cur: nums){
+            if (cur == 0) zeros ++;
+            else total *= cur; 
         }
-        for (int i=n-1;i>=0;i--){
-            b[i] = b[i+1] * nums[i];
-        }
+        for (int cur: nums){
+            
+            if (cur == 0) {
+                if (zeros == 1) ans.push_back(total);
+                else ans.push_back(0);
+            }else{
+                if (zeros > 0){
+                    ans.push_back(0);
+                }else{
+                    ans.push_back(total / cur);
+                }
+            }
         
-        for (auto i : a )cout << i << " " ;
-        cout << endl;
-        
-        for (auto i : b)cout << i << " " ;
-        cout << endl;
-        
-        
-        for (int i=0;i<n;i++){
-            ans[i] = (a[i]*b[i+1]);
         }
         return ans;
+        
+        
     }
 };

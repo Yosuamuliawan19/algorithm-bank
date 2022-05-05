559ce86254c32024548365185e809a124a5051cb
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2022-05-05 13:39:38 +0000

    Sync LeetCode submission - Group Anagrams (cpp)

--- Leetcode_submissions/problems/group_anagrams/solution.cpp
+++ Leetcode_submissions/problems/group_anagrams/solution.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string, int> mp;
-        vector<vector<string>> ans;
+        map<string, int> mp; 
         int idx = 0;
-        for (int i=0;i<strs.size();i++){
-            string a = strs[i];
-           
-            sort(a.begin(), a.end());
-             cout << a << " " << strs[i] << endl;
-            if (mp.find(a) != mp.end()){
-                ans[mp[a]].push_back(strs[i]);
+        vector<vector<string>> ans;
+        
+        for (string cur: strs){
+            string scur = cur;
+            sort(scur.begin(), scur.end());
+            
+            if (mp.find(scur) != mp.end()){
+                ans[mp[scur]].push_back(cur);
             }else{
-                mp[a] = idx++;
-                ans.push_back(vector<string> ());
-                ans[idx-1].push_back(strs[i]);
+                mp[scur] = idx++;
+                ans.push_back(vector<string>());
+                ans[idx-1].push_back(cur);
             }
         }
         return ans;
+        
     }
 };

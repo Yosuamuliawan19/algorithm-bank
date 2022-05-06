99e4d78446adceb3cc7da4a258cb607b82d29867
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2022-05-06 05:29:43 +0000

    Sync LeetCode submission - Valid Sudoku (cpp)

--- Leetcode_submissions/problems/valid_sudoku/solution.cpp
+++ Leetcode_submissions/problems/valid_sudoku/solution.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int n = 9, col[9] = {0}, row[9] = {0}, quad[9][9] = {0};
+        // arrays to store visited numbers for each row, columns, and each quadrant
+        int n = 9, row[9] = {0}, col[9] = {0}, q[9][9] = {0};
+        
         for (int i=0;i<n;i++){
-            for (int j=0;j<n;j++){
+            for(int j=0;j<n;j++){
                 if (board[i][j] != '.'){
-                    if (++ col[board[i][j] -'0'-1] > 1) return false;    
-                    int curquad = floor(j/3) * 3 + floor(i/3);
-                    if (++ quad[curquad][board[i][j] -'0'-1] > 1) return false;     
-                }      
+                    // check if row already has this number
+                    if (++row[board[i][j] - '0' - 1] > 1) return false;                    
+                    
+                    // check if quadrant already has this number
+                    int quad = floor(j/3) * 3 + floor(i/3);
+                    if (++q[quad][board[i][j] - '0' - 1] > 1) return false;
+                }
                 if (board[j][i] != '.'){
-                    if (++ row[board[j][i] -'0'-1] > 1) return false;
-                }  
+                    // check if column already has this number
+                    if (++col[board[j][i] - '0' - 1] > 1) return false;
+
+                }
+                
             }
-            memset(col, 0, sizeof(col)); memset(row, 0, sizeof(row));
-        }        
+            // reset row and column
+            memset(row, 0, sizeof(row));
+            memset(col, 0, sizeof(col));
+        }
+        
         return true;
     }
 };

b3b5c71b3e9d267042a704f7d1e1beed9c1bbc3e
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2022-05-07 07:37:10 +0000

    Sync LeetCode submission - House Robber (cpp)

--- Leetcode_submissions/problems/house_robber/solution.cpp
+++ Leetcode_submissions/problems/house_robber/solution.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-
     int rob(vector<int>& nums) {
-        if (nums.size() == 0) return 0; 
-        if (nums.size() == 1) return nums[0];
-        if (nums.size() == 2){
-            return max(nums[0], nums[1]);
-        }
-        vector<int> arr(nums.size());
-        
+        vector<int> arr =  vector<int>(nums.size(), 0);
         arr[0] = nums[0];
-        arr[1] = nums[1];
-        arr[2] = nums[0] + nums[2];
-        for (int i=3;i<nums.size();i++){
-            arr[i] = nums[i] + max(arr[i-2], arr[i-3]);
+        if (nums.size() >= 2) arr[1] = nums[1];
+        for (int i=2;i<nums.size();i++){
+            if (i == 2){
+                arr[i] = nums[i] + arr[i-2];
+            }else{
+                arr[i] = max(nums[i] + arr[i-3], nums[i] + arr[i-2]);
+            }
         }
-        int mx = 0;
-        for (auto i: arr){
-            mx = max(i, mx);
+        
+        
+//         for (int i: arr){
+//             cout << i << endl;
+//         }
+        
+        
+        
+        int n = nums.size()-1, idx = 0 ;
+        int ans = 0;
+        while (n != -1 && idx < 3){
+            ans = max(ans, arr[n]);
+            n--;idx++;
         }
-        return mx;
+        return ans;
+        
     }
 };

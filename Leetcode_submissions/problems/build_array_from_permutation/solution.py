3571ed5a116865aaef9380f9024333971170156c
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2022-05-08 17:03:02 +0000

    Sync LeetCode submission - Build Array from Permutation (python3)

---
+++ Leetcode_submissions/problems/build_array_from_permutation/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        ans = []
+        
+        for i in nums:
+            ans.append(nums[i])
+            
+        return ans

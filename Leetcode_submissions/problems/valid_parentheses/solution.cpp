2974ed62ca197ac42796431007fc49db98c6f108
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2022-05-08 10:30:53 +0000

    Sync LeetCode submission - Valid Parentheses (cpp)

--- Leetcode_submissions/problems/valid_parentheses/solution.cpp
+++ Leetcode_submissions/problems/valid_parentheses/solution.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> q;
-        bool f = 1;
+        stack<char> st;
         for (int i=0;i<s.length();i++){
-            if (s[i] == '(' || s[i] == '[' || s[i] == '{') q.push(s[i]);
-            else if (s[i] == ')'){
-                if (q.empty() || q.top() != '('){
-                    f = 0;break;
-                }
-                q.pop();
-            }else if  (s[i] == '}'){
-                if (q.empty() || q.top() != '{'){
-                    f = 0;break;
-                }
-                q.pop();
-            }else{
-                if (q.empty() || q.top() != '['){
-                    f = 0;break;
-                }
-                q.pop();
+            if (st.empty()) {
+                st.push(s[i]);
+                continue;
             }
-        } 
-        if (!q.empty()) f = 0;
-        return f;
+            // cout << st.top() << " " << s[i] << endl;            
+            if (st.top() == '{' && s[i] == '}'){
+                st.pop(); continue;
+            } else if (st.top() == '(' && s[i] == ')'){
+                st.pop(); continue;
+            } else  if (st.top() == '[' && s[i] == ']'){
+                st.pop(); continue;
+            }else st.push(s[i]);
+        }
+        // cout << st.size() << st.top() <<  endl;
+        return st.empty();
     }
 };

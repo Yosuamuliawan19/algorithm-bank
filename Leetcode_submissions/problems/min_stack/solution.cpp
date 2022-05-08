a37671f2b41c793644e36b2e7544d9f8b9cfb431
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2022-05-08 10:57:01 +0000

    Sync LeetCode submission - Min Stack (cpp)

--- Leetcode_submissions/problems/min_stack/solution.cpp
+++ Leetcode_submissions/problems/min_stack/solution.cpp
@@ -1,38 +1,39 @@
 class MinStack {
+    
+    int idx;
+     vector<pair<int, int>> arr;
+    
 public:
-    /** initialize your data structure here. */
-    vector<int> v = vector<int>(1);
-    int idx = 0;
     MinStack() {
-        
+        idx = 0;
+        arr = vector<pair<int,int>>();
     }
     
-    void push(int x) {
-        v.push_back(0);
-        v[idx++] = x;
+    void push(int val){
+        pair<int,int> node = make_pair(val, min(this->getMin(), val));
+        if (idx == arr.size()) arr.push_back(node);
+        else arr[idx] = node;
+        idx++;
     }
     
-    void pop() {
-        idx--;
+    void pop() {   
+        idx --;
     }
     
     int top() {
-        return v[idx-1];
+        return arr[idx-1].first;
     }
     
     int getMin() {
-        int mn = INT_MAX;
-        for (int i=0;i<idx;i++){
-            mn = min(mn, v[i]);
-        }
-        return mn;
+        if (idx == 0) return INT_MAX;
+        return arr[idx-1].second;
     }
 };
 
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack* obj = new MinStack();
- * obj->push(x);
+ * obj->push(val);
  * obj->pop();
  * int param_3 = obj->top();
  * int param_4 = obj->getMin();

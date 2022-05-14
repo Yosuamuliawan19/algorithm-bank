f224c2ce322f66ea7044fec92eacb53383e5075d
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2022-05-14 14:09:51 +0000

    Sync LeetCode submission - Min Stack (python3)

---
+++ Leetcode_submissions/problems/min_stack/solution.py
@@ -0,0 +1,27 @@
+class MinStack:
+
+    def __init__(self):
+        self.st = []    
+    
+    def push(self, val: int) -> None:
+        if not self.st:
+            self.st.append((val,val))
+        else:
+            self.st.append((val, min(val, self.st[-1][1])))
+        
+    def pop(self) -> None:
+        self.st.pop()                
+
+    def top(self) -> int:
+        return self.st[-1][0]
+
+    def getMin(self) -> int:
+        return self.st[-1][1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()

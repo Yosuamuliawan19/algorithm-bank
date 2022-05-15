2dcb00bd7cee9fa1e992f2a3592cd6d681e7d602
Yosuamuliawan19 <yosuamuliawan19@gmail.com>
2022-05-15 13:36:20 +0000

    Sync LeetCode submission - Palindrome Number (python3)

---
+++ Leetcode_submissions/problems/palindrome_number/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        
+        x = str(x)
+        l, r = 0, len(x)-1
+        
+        
+        while r > l:
+            if x[l] != x[r]:
+                return False
+            l+=1
+            r-=1
+        return True
